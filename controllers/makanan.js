ee5fde11bd0158f1abee7dc7d5e221eeb2299173
unknown <afiq20030602@gmail.com>
2023-12-31 12:25:00 +0700

    all food database

--- controllers/makanan.js
+++ controllers/makanan.js
@@ -3,29 +3,28 @@ const User = require('../models/user');
 
 module.exports.renderDashboard = (req, res) => {
     res.render('dashboard/dashboard');
-}
+}   
 module.exports.renderDeskripsi = async (req, res) => {
     data = await Makanan.find({ id: req.params.id });
     data = data[0];
     res.render('dashboard/deskripsi', { data });
-}
+}   
 module.exports.renderResep = async (req, res) => {
     data = await Makanan.find({ id: req.params.id });
     data = data[0];
     res.render('dashboard/deskripsi', { data });
     res.render('dashboard/resep', { data });
-}
+}   
 module.exports.renderProsedur = async (req, res) => {
     data = await Makanan.find({ id: req.params.id });
     data = data[0];
     res.render('dashboard/deskripsi', { data });
     res.render('dashboard/prosedur', { data })
-}
+}   
 module.exports.renderListMakanan = async (req, res) => {
     allMakanan = await Makanan.find({});
-    res.render('dashboard/show-makanan', { dataMakanan: allMakanan});
-}
-
+    res.render('dashboard/show-makanan', { dataMakanan: allMakanan });
+}   
 module.exports.renderCariMakanan = (req, res) => {
     res.render('dashboard/cari')
 }
--- models/makanan.js
+++ models/makanan.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MakananSchema = new Schema({
-    id: Number,
-    namal: String,
+    nama_makanan: String,
     deskripsi: String,
     path_gambar: String,
     click_count: Number,
